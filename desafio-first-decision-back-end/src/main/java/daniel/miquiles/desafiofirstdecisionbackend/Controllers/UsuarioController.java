4250e5e5b009d8b8330638a56e230ae9563db852
Daniel Miquiles <danielvictormiquiles@gmail.com>
2023-09-26 14:00:15 -0300

    fix: corrigindo controller

---
+++ desafio-first-decision-back-end/src/main/java/daniel/miquiles/desafiofirstdecisionbackend/Controllers/UsuarioController.java
@@ -0,0 +1,43 @@
+package daniel.miquiles.desafiofirstdecisionbackend.Controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import daniel.miquiles.desafiofirstdecisionbackend.Dto.UsuarioRequestDTO;
+import daniel.miquiles.desafiofirstdecisionbackend.Dto.UsuarioResponseDTO;
+import daniel.miquiles.desafiofirstdecisionbackend.Models.Usuario;
+import daniel.miquiles.desafiofirstdecisionbackend.Services.UsuarioService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+@RestController()
+@Validated
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<UsuarioResponseDTO> listar() {
+        return usuarioService.listar();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public UsuarioResponseDTO create(@RequestBody @Valid @NotNull UsuarioRequestDTO usuarioDTO) {
+        return usuarioService.create(usuarioDTO);
+    }
+    
+}
