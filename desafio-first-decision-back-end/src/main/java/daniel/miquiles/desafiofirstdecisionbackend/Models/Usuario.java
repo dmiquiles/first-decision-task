f6996139a0dbcf3d73ae1b4498bf419df04238e4
Daniel Miquiles <danielvictormiquiles@gmail.com>
2023-09-26 10:08:17 -0300

    feat: configurando JPA e adicionando validações

--- desafio-first-decision-back-end/src/main/java/daniel/miquiles/desafiofirstdecisionbackend/Models/Usuario.java
+++ desafio-first-decision-back-end/src/main/java/daniel/miquiles/desafiofirstdecisionbackend/Models/Usuario.java
@@ -1,15 +1,36 @@
 package daniel.miquiles.desafiofirstdecisionbackend.Models;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @Entity
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank
+    @Length(min = 3, max = 50)
+    @Column(length = 51, nullable = false)
     String nome;
+    
+    @NotBlank
+    @Email
     String email;
+
+    @NotBlank
+    @Length(min = 6, max = 20)
+    @Column(length = 51, nullable = false)
     String senha;
 
 }
