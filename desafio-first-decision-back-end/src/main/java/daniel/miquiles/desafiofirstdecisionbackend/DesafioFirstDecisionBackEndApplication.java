b0270912d46251d19dec60120d008be5ebdeed1a
Daniel Miquiles <danielvictormiquiles@gmail.com>
2023-09-26 09:49:50 -0300

    feat: primeiro commit

---
+++ desafio-first-decision-back-end/src/main/java/daniel/miquiles/desafiofirstdecisionbackend/DesafioFirstDecisionBackEndApplication.java
@@ -0,0 +1,13 @@
+package daniel.miquiles.desafiofirstdecisionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFirstDecisionBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFirstDecisionBackEndApplication.class, args);
+	}
+
+}
---
+++ desafio-first-decision-back-end/src/test/java/daniel/miquiles/desafiofirstdecisionbackend/DesafioFirstDecisionBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.miquiles.desafiofirstdecisionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFirstDecisionBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
