13e0208a64df765048a2538767007c3c45446356
Daniel Miquiles <danielvictormiquiles@gmail.com>
2023-09-27 20:52:39 -0300

    fix: corrigindo scss, rotas e testes

--- first-frontend/src/app/app-routing.module.ts
+++ first-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListarUsuariosComponent
+  },
+  {
+    path: 'cadastrar',
+    component: CadastroUsuarioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- first-frontend/src/app/app.module.ts
+++ first-frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+import { FormUsuarioComponent } from './components/form-usuario/form-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroUsuarioComponent
+    CadastroUsuarioComponent,
+    ListarUsuariosComponent,
+    FormUsuarioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
